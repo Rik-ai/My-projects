e4b5ce9d69cb4012352a37ae6f071a6e8729697b
Ivan <ivan.lakhanin@gmail.com>
2020-02-05 16:48:51 +0200

    Control Flow - logical operators

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,22 @@
-// else if statements
+//logical operators - OR || and AND &&
 
-// const age = 25;
+const password = 'p@ssword';
 
-// if (age > 20) {
-//     console.log('you are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if (ninjas.length > 3) {
-
-//     console.log("that's a lot of ninjas");
-// }
-
-const password = 'p@ssword1234';
-/*множественный цикл который проверяет разные условия по порядку*/
-if (password.length >= 12) {
+if (password.length >= 12 && password.includes('@')) {
     console.log('that password is a strong');
-} else if (password.length >= 8) {
+} else if (password.length >= 8 || password.includes('@') && password.length > 5) {
     console.log(' that password is long enough!');
 } else {
     console.log('password is not long enough!');
 }
+
+//logical NOT (!)
+
+let user = false; /*если у нас user =false, с помощью (!) мы можем изменить на user=true*/
+
+if (!user) {
+    console.log('you must be logged in to continue');
+}
+
+console.log(!true);
+console.log(!false);
