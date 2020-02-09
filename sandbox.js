85e054e551e9aeb30338b3857518724e2c57c61d
Ivan <ivan.lakhanin@gmail.com>
2020-02-09 18:33:37 +0200

    Functions & Methods - Functions vs Methods

--- sandbox.js
+++ sandbox.js
@@ -1,48 +1,15 @@
-// regular function
-// const calcArea = function (radius) {
-//     return 3.14 * radius ** 2;
-
-// };
-
-//arrow function - функция стрелы проще в создании и и более короткий способ написания по сравнению с регулярной функцией
-const calcArea = (radius, something) => 3.14 * radius ** 2; /*если у нас 1 параметр, скобки () не нужны, если больше, то нужны, так же не нужнжы {} если у нас однострочное выражение, и нам не нужно ключевое слово return, т.к. оно и так будет действовать автоматически*/
-const area = calcArea(5);
-console.log('area is', area);
+const name = 'shaun';
 
 
+//functions - определены на обьекте или типе данных в самой фнкции
+
+const greet = () => "hello";
+
+let resultOne = greet();
+console.log(resultOne);
 
 
-//practise arrow function
-//1
-const greet = function () {
-    return 'hello, world';
-};
+//methods - определены на обьекте или типе данных
 
-const greet = () => 'hello,world';
-const result = greet();
-console.log('result is:', result)
-
-
-
-
-//2
-const bill = function (products, tax) {
-    let total = 0;
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-}
-
-
-const bill = (products, tax) => {
-    let total = 0;
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-};
-
-console.log(bill([10, 15, 30], 0.2));
-
-
+let resultTwo = name.toUpperCase(); /*то же функция только называется методом с использованием точечной записи*/
+console.log(resultTwo);
