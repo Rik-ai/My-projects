08b8af18db03e94dd8f4b8656686f27ff439e8b3
Ivan <ivan.lakhanin@gmail.com>
2020-02-09 17:23:35 +0200

    Functions & Methods - Arguments & Parameters

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,10 @@
-// functionn declaration - мы можем опстить эту функцию под её вывод (greet), но она всё равно будет выполнятся, так как JS поднимает её на верх, в этом и разница между functionn declaration и function expression, т.к. последняя не будет так работать
-function greet() { /*greet - просто название функциии*/
-    console.log('hello there');
-}
+// arguments & parameter
 
+const speak = function (name = 'ivan', time = 'night') {
+    console.log(`good ${time} ${name}`);
 
-//function expression
-const speak = function () { /*функция выражения, когда мы храним функцию внутри переменной.*/
-    console.log('good day');
 };
 
 
-// greet();
-// greet();
-// greet();
-
-
 speak();
-speak();
-speak();
-speak();
+speak('shaun', 'day');
