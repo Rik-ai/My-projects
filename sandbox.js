8d4a764f9e5aaa6e4614b8e0070268826357c898
Ivan <ivan.lakhanin@gmail.com>
2020-02-09 17:11:26 +0200

    Functions & Methods - Function Declarations & Expressions

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,21 @@
-// variable & block scope/переменные и области блока
-let age = 30;
-
-//let age = 50; /*так не получится переопределить переменную*/
-
-if (true) {
-    //age = 40; /*так мы не переопределяем переменную а просто изменяем её обращяясь к let age = 30; (изменяется в обоих блоках кода)*/
-    let age = 40; /*так можно переопределить переменную внутри блока кода, но не снаружи*/
-    let name = 'shaun';
-    console.log('inside 1st code block: ', age, name); /*мы можем получить доступ к переменной которая находится в глобальной области видимости в не кода*/
-    if (true) {
-        let age = 50;
-        console.log('inside 2st code block: ', age);
-
-    }
-
-
+// functionn declaration - мы можем опстить эту функцию под её вывод (greet), но она всё равно будет выполнятся, так как JS поднимает её на верх, в этом и разница между functionn declaration и function expression, т.к. последняя не будет так работать
+function greet() { /*greet - просто название функциии*/
+    console.log('hello there');
 }
 
-console.log('outside code block: ', age, name);
+
+//function expression
+const speak = function () { /*функция выражения, когда мы храним функцию внутри переменной.*/
+    console.log('good day');
+};
+
+
+// greet();
+// greet();
+// greet();
+
+
+speak();
+speak();
+speak();
+speak();
