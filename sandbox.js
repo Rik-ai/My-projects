ed18a28e527d742e3ee1719879bd996ad7f0be87
Ivan <ivan.lakhanin@gmail.com>
2020-02-09 20:05:09 +0200

    Objects - object literals

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,21 @@
-//get a reference to the 'ul'
-const ul = document.querySelector('.people');
+//object literals
 
 
-const people = ['mario', 'luigi', 'rui', 'shaun', 'chun-li'];
+let user = { /*Это обьект*/
+    name: 'crystal', /*Это свойство состоящее из ключа (name) и значения ('crystal')*/
+    age: 30,
+    email: 'crystal@gmail.com',
+    location: 'kyiv',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+}
 
-let html = ``;
+console.log(user);
+console.log(user.name); /*получим доступ к свойству name*/
 
-people.forEach(person => {
-    //create html template
-    html += `<li style="color: purple">${person}</li>` /*для каждого имени добавляем фрагмент этого шаблона html*/
-});
 
-console.log(html);
-ul.innerHTML = html;
+user.age = 35;/*можем изменить или обновить свойство обьекта*/
+console.log(user.age);
+
+console.log(user['email']); /*так же можно получить доступ к свойству*/
+user['name'] = 'chun-li'; /*так же можем изменить или обновить свойство обьекта*/
+console.log(user['name']);
