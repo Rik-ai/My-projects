9fd25b7c66d9f3de558c14f79eba609199c665c8
Ivan <ivan.lakhanin@gmail.com>
2020-02-09 21:23:32 +0200

    Objects - Math object

--- sandbox.js
+++ sandbox.js
@@ -1,41 +1,24 @@
-//Objects in Arrays
-/*теперь представим блоги не как строки, а как обьекты*/
-// const blogs = [
-//     { title: 'why mac & cheese rules', likes: 30 }, /*первый обьект в массиве*/
-//     { title: '10 things to make with marmite', likes: 50 }
-// ];
-
-//console.log(blogs);
+// Math object
 
 
-let user = { /*Это обьект*/
-    name: 'crystal', /*Это свойство состоящее из ключа (name) и значения ('crystal')*/
-    age: 30,
-    email: 'crystal@gmail.com',
-    location: 'kyiv',
-    blogs: [ /*теперь представим блоги не как строки, а как обьекты*/
-        { title: 'why mac & cheese rules', likes: 30 },
-        { title: '10 things to make with marmite', likes: 50 }
-    ],
-    /*methods*/
-    login() {
-        console.log('the user logged in');
-    },
-    logout() {
-        console.log('the user logged out');
-    },
-    logBlogs() { /*для того что бы написать код проще можно убрать :function*/
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog => { /*this - ключевое слово устанавливает быть обьектом*/
-            console.log(blog.title, blog.likes); /*теперь каждый блог представлен как обьект через цикл*/
-        })
-    }
-};
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-user.logBlogs();
-console.log(this);
-user.login();
-user.logout();
 
-const name = 'mario';
-name.toUpperCase();
+
+const area = 5.5;
+
+
+console.log(Math.round(area)); /*round - округление числа*/
+console.log(Math.floor(area)); /* floor - округляет в меньшую сторону*/
+console.log(Math.ceil(area)); /*ceil - округляет в большую сторону*/
+console.log(Math.trunc(area)); /*trunc - убирает остаток*/
+
+
+//random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
