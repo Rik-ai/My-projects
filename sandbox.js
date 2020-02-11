e49507b831389a5426f172dc975443a67699c273
Ivan <ivan.lakhanin@gmail.com>
2020-02-11 21:51:16 +0200

    The DOM - Event Bubbling (and delegation)

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,31 @@
 const ul = document.querySelector('ul');
-//ul.remove(); /*удаляет елементы из тега*/
-
-
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    //1й метод добавления
     const li = document.createElement('li');
     li.textContent = 'something new to do';
-    ul.append(li);/* этот метод берёт тег (li) и отправляет его в самый ВЕРХ родителя т.е. когда мы нажимаем на кнопку добавляется новая информация на страницу снизу*/
-    ul.prepend(li);/*этот метод берёт тег (li) и отправляет его в самый НИЗ родителя т.е. когда мы нажимаем на кнопку добавляется новая информация на страницу сверху*/
-
-    //2й метод добавления
-    //ul.innerHTML += '<li>something new</li>'; /*когда мы нажимаем на кнопку добавляется новая информация на страницу*/
+    ul.prepend(li);
 });
 
 
-const items = document.querySelectorAll('li');
+// const items = document.querySelectorAll('li');
 
-items.forEach(item => {
-    item.addEventListener('click', event => {
+// items.forEach(item => {
+//     item.addEventListener('click', event => {
+//         console.log('events in LI');
+//         event.stopPropagation();
+//         event.target.remove(); /*удаляет елемент на странице когда мы на него нажимаем*/
 
-        //event.target.style.textDecoration = 'line-through'; /*позволяет добавить какой либо эффект когда мы нажимаем на кнопку*/
-        event.target.remove(); /*удаляет елемент на странице когда мы на него нажимаем*/
+//     });
+// });
 
-    });
+ul.addEventListener('click', e => { /*теперь новые теги (li) добавляются так как будто они прописаны в HTML файле*/
+    // console.log('events in UL');
+    console.log(e.target);
+    if (e.target.tagName === 'LI') {
+        e.target.remove();
+    }
 });
+
+
+//ТОЛКОМ НЕ ПОНЯЛ ЭТОТ УРОК
