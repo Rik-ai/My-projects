7915a0cc74cc746cc504b241de868fcb2742e386
Ivan <ivan.lakhanin@gmail.com>
2020-02-11 21:08:02 +0200

    The DOM - Event Basics (click events)

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,16 @@
-//Relationship Parents-Child
+// const button = document.querySelector('button');
 
-const article = document.querySelector('article'); /*так как это будет HTMLCollection мы не можем применять forEach, необходимо преобразовать в массив*/
-// console.log(article.children);
-
-// Array.from(article.children); /*не обязательно писать отдельными строками, можно сразу в консольлог*/
-// console.log(Array.from(article.children));/*это преобразование не деструктивный метод, это новое значение*/
-// console.log(article.children);/*проверка, что это всё ещё HTMLCollection*/
-
-// Array.from(article.children).forEach(child => {
-//     child.classList.add('article-element');
+// button.addEventListener('click', () => { /*.addEventListener - определяет какое событие происходит*/
+//     console.log('you clicked me');
 // });
 
-const title = document.querySelector('h2');
+const items = document.querySelectorAll('li');
 
-console.log(title.parentElement); /*.parentElement находит родителя элемента*/
-console.log(title.parentElement.parentElement); /*находит родителя, елемента родителя(Родитель->родитель->ребёнок)*/
-
-//Siblings - братья и сёстры
-console.log(title.nextElementSibling); /*находит следующего ребёнка родителя*/
-console.log(title.previousElementSibling);/*находит предыдущего ребёнка родителя*/
-
-
-//chaining
-
-console.log(title.nextElementSibling.parentElement.children);
+items.forEach(item => {
+    item.addEventListener('click', event => {
+        //console.log('item click');
+        //console.log(event);
+        console.log(event.target); /*.target - определяет какую конкретно мы нажали кнопку*/
+        event.target.style.textDecoration = 'line-through'; /*позволяет добавить какой либо эффект когда мы нажимаем на кнопку*/
+    });
+});
