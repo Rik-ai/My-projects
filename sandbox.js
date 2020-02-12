b533e67e7ccd30251bff84abf826bc42fb48f57e
Ivan <ivan.lakhanin@gmail.com>
2020-02-12 15:20:20 +0200

    Forms & Form Events - Testing RegEx Patterns

--- sandbox.js
+++ sandbox.js
@@ -6,3 +6,27 @@ form.addEventListener('submit', e => {
     // console.log(username.value);/*выводит в консоли юсернейм*/
     console.log(form.username.value);/*можно так написать быстрее так как у нас в теге form находится input с id=username, так же допустим можно изменить id на name=username, будет работать так же*/
 });
+
+
+
+//testing RegEx
+const username = 'dfsshftyh';
+/*регулярные выражения пишутся в /  / */
+const pattern = /^[a-z]{6,}$/; /*создаём шаблон с параметрами ввода, если в начале и конце параметров поставить ^ и $ то имя не должно содержать лишних символов кроме букв*/
+
+//1й метод
+// let result = pattern.test(username);
+// console.log(result);
+
+// if (result) {
+//     console.log('regex test passed :)');
+// } else {
+//     console.log('regex test failed :(');
+
+// }
+
+
+//2й метод
+
+let result = username.search(pattern);
+console.log(result)
