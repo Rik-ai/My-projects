5f39431788524ae1ffef2abf185918f190fda5e8
Ivan <ivan.lakhanin@gmail.com>
2020-02-12 15:43:24 +0200

    Forms & Form Events - Basic Form Validation

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,25 @@
 const form = document.querySelector('.signup-form');
-//const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
-    e.preventDefault();/*страница не обновляется при нажатии на кнопку*/
-    // console.log(username.value);/*выводит в консоли юсернейм*/
-    console.log(form.username.value);/*можно так написать быстрее так как у нас в теге form находится input с id=username, так же допустим можно изменить id на name=username, будет работать так же*/
+    e.preventDefault();
+
+    //validetion
+
+    const username = form.username.value;
+    const usernamePattern = /^[a-z A-Z]{6,12}$/;
+
+    if (usernamePattern.test(username)) {
+        //feedback good info
+        feedback.textContent = 'that username is valid!';
+    } else {
+        //feedback help info
+        feedback.textContent = 'username must contain letters only & be between 6 & 12 characters long';
+    }
 });
 
 
 
-//testing RegEx
-const username = 'dfsshftyh';
-/*регулярные выражения пишутся в /  / */
-const pattern = /^[a-z]{6,}$/; /*создаём шаблон с параметрами ввода, если в начале и конце параметров поставить ^ и $ то имя не должно содержать лишних символов кроме букв*/
-
-//1й метод
-// let result = pattern.test(username);
-// console.log(result);
-
-// if (result) {
-//     console.log('regex test passed :)');
-// } else {
-//     console.log('regex test failed :(');
-
-// }
 
 
-//2й метод
 
-let result = username.search(pattern);
-console.log(result)
