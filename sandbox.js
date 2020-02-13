ec59ae6649dfee01ee0df2b86b918318dc57ccd9
Ivan <ivan.lakhanin@gmail.com>
2020-02-13 16:07:04 +0200

    Array Methods - Find method

--- sandbox.js
+++ sandbox.js
@@ -1,46 +1,9 @@
-//Reduce method
+//Find method - находит в массиве первый заданый параметр соответствующий условиям поиска
 
-//const scores = [10, 20, 60, 40, 70, 90, 30];
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
+const firstHighScore = scores.find((score) => {
+    return score > 50;
+});
 
-// const result = scores.reduce((acc, curr) => { /*First of all the accumulator then current*/
-//     if (curr > 50) {
-//         acc++;
-//     }
-//     return acc;
-// }, 0);
-
-// console.log(result);
-
-
-
-
-const scores = [
-    { player: 'mario', score: 50 },
-    { player: 'yoshi', score: 30 },
-    { player: 'mario', score: 10 },
-    { player: 'crystal', score: 60 },
-    { player: 'mario', score: 50 },
-    { player: 'yoshi', score: 30 },
-    { player: 'mario', score: 50 },
-    { player: 'crystal', score: 60 },
-    { player: 'mario', score: 50 },
-    { player: 'yoshi', score: 30 },
-    { player: 'mario', score: 80 },
-    { player: 'crystal', score: 60 },
-    { player: 'mario', score: 50 },
-    { player: 'yoshi', score: 30 },
-    { player: 'mario', score: 0 },
-    { player: 'crystal', score: 60 },
-];
-
-const marioTotal = scores.reduce((acc, curr) => {
-    if (curr.player === 'mario') {
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
-
-console.log(marioTotal);
-
-//не особо понял с acc и curr херово с инглишем)
+console.log(firstHighScore);
