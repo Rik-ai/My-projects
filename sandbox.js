08c4739f5961e638270f27fa674493f9e2edd1ea
Ivan <ivan.lakhanin@gmail.com>
2020-02-13 16:46:48 +0200

    Array Methods - Chaining Array Methods

--- sandbox.js
+++ sandbox.js
@@ -1,40 +1,20 @@
-//example 1 - sorting strings - сортирует автоматически строки по алфавиву, деструктивный метод
-const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
+//Chaining Array Methods
 
-names.sort();
-names.reverse(); /*меняет порядок в массиве на обратный*/
-console.log(names);
-
-//example 2 - sorting numbers сортирует числа по возрастанию, но смотрит только на первую цифру в числе
-const scores = [10, 50, 20, 5, 35, 70, 45];
-scores.sort();
-console.log(scores);
-
-scores.sort((a, b) => b - a); /*сортирует числа по убыванию*/
-scores.reverse();
-console.log(scores);
-
-//example 3 - sorting objects
-const players = [
-    { name: 'mario', score: 20 },
-    { name: 'luigi', score: 10 },
-    { name: 'chun-li', score: 50 },
-    { name: 'yoshi', score: 30 },
-    { name: 'shaun', score: 70 }
+const products = [
+    { name: 'gold star', price: 30 },
+    { name: 'green shell', price: 10 },
+    { name: 'red shell', price: 40 },
+    { name: 'banana', price: 5 },
+    { name: 'mushrooms', price: 50 }
 ];
 
-players.sort((a, b) => {
-    if (a.score > b.score) {
-        return -1; /*если ТРУ то -1*/
-    } else if (b.score > a.score) {
-        return 1;/*иначе мы хотим поменять их местами*/
-    } else {
-        return 0;
-    }
-});
+// const filtered = products.filter(product => product.price > 20);
 
-//короткая запись
-players.sort((a, b) => {
-    b.score - a.score
-});
-console.log(players);
+// const promos = filtered.map(product => {
+//     return `the${product.name} is ${product.price / 2} pounds`;
+// });
+
+const promos = products
+    .filter(product => product.price > 20)
+    .map(product => `the${product.name} is ${product.price / 2} pounds`);
+console.log(promos);
