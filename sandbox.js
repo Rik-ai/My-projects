7c9f35793813ef7fddd6e56a64606b8148e1eb78
Ivan <ivan.lakhanin@gmail.com>
2020-02-19 21:44:15 +0200

    Dates & Times - Date-fns Library

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,16 @@
-const clock = document.querySelector('.clock');
+const now = new Date();
 
-const tick = () => {
-    const now = new Date();
+//console.log(dateFns.isToday(now));
 
-    const h = now.getHours();
-    const m = now.getMinutes();
-    const s = now.getSeconds();
 
-    const html = `
-    <span>${h}</span> : 
-    <span>${m}</span> : 
-    <span>${s}</span>    
-    `;
+//formatting options
+console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, 'MMMM'));
+console.log(dateFns.format(now, 'dddd'));
+console.log(dateFns.format(now, 'Do'));
+console.log(dateFns.format(now, 'dddd Do MMMM YYYY'));
 
-    clock.innerHTML = html;
+//comparing dates
+const before = new Date('February 1 2020 12:00:00');
 
-};
-
-setInterval(tick, 1000);
+console.log(dateFns.distanceInWords(now, before, { addSuffix: true }));
