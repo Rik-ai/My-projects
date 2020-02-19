c44b0c837f2d36861028f4729d5b02cf2469369b
Ivan <ivan.lakhanin@gmail.com>
2020-02-19 21:27:37 +0200

    Dates & Times - Building a Digital Clock

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,20 @@
-// timestamps
+const clock = document.querySelector('.clock');
 
-const before = new Date('February 1 2020 7:30:25'); //дата создания чего-либо
-const now = new Date();
+const tick = () => {
+    const now = new Date();
 
-console.log(now.getTime(), before.getTime());
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
 
+    const html = `
+    <span>${h}</span> : 
+    <span>${m}</span> : 
+    <span>${s}</span>    
+    `;
 
-const diff = now.getTime() - before.getTime(); //сколько времени прошло с момента создания
-console.log(diff);
+    clock.innerHTML = html;
 
-const mins = Math.round(diff / 1000 / 60); //перевод милисекунд в минуты
-const hours = Math.round(mins / 60);
-const days = Math.round(hours / 24);
+};
 
-
-console.log(mins, hours, days);
-
-console.log(`the что-то was created ${days} ago`);
-
-
-//converting timestamps into date objects
-
-const timestamp = 1675938474990;
-console.log(new Date(timestamp));
+setInterval(tick, 1000);
