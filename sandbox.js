84834b23f850adbe0bfa2d7956c596b84189b3bf
Ivan <ivan.lakhanin@gmail.com>
2020-02-20 15:22:34 +0200

    Async JavaScript - Callback Functions

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,32 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
 
-request.addEventListener('readystatechange', () => {
-    //console.log(request, request.readyState);
-    if (request.readyState === 4) {
-        console.log(request.responseText);
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        //console.log(request, request.readyState);
+        if (request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        } else if (request.readyState === 4) {
+            callback('coud not fetch data', undefined);
+        }
+    });
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); //первый аргумен это тип запроса, второй аргумент это откуда мы хотим получить запрос, конечная точка откуда хотим получить данные
+    request.send();
+
+};
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+    console.log('callback fired');
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); //первый аргумен это тип запроса, второй аргумент это откуда мы хотим получить запрос, конечная точка откуда хотим получить данные
-request.send();
+console.log(3);
+console.log(4);
