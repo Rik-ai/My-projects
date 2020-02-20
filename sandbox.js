5f26ea0c6c64b6ecd4c2c29b0f11d3f21a44bde6
Ivan <ivan.lakhanin@gmail.com>
2020-02-20 14:28:06 +0200

    Async JavaScript - Making HTTP Requests (XHR)

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,11 @@
-console.log(1);
-console.log(2);
+const request = new XMLHttpRequest();
 
-setTimeout(() => {
-    console.log('calback function fired') //задержка кода для наглядной демонстрации асинхронного кода
-}, 2000);
+request.addEventListener('readystatechange', () => {
+    //console.log(request, request.readyState);
+    if (request.readyState === 4) {
+        console.log(request.responseText);
+    }
+});
 
-
-console.log(3);
-console.log(4);
-//главный принцып асинхронного кода в том, что бы выполнять код на который нужно много времени позже после выполнения другого кода
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); //первый аргумен это тип запроса, второй аргумент это откуда мы хотим получить запрос, конечная точка откуда хотим получить данные
+request.send();
