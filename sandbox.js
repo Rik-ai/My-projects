f3419fe25e4dcbe48ba1303ab352880b6e0b7710
Ivan <ivan.lakhanin@gmail.com>
2020-02-20 15:51:31 +0200

    Async JavaScript - JSON Data

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,19 @@
 const getTodos = (callback) => {
 
-    const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest(); //запрос с сервера
 
     request.addEventListener('readystatechange', () => {
         //console.log(request, request.readyState);
         if (request.readyState === 4 && request.status === 200) {
-            callback(undefined, request.responseText);
+            const data = JSON.parse(request.responseText);
+            /*JavaScript Object Notation превращяет JS обьекты в JSON строки*/
+            callback(undefined, data);
         } else if (request.readyState === 4) {
             callback('coud not fetch data', undefined);
         }
     });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); //первый аргумен это тип запроса, второй аргумент это откуда мы хотим получить запрос, конечная точка откуда хотим получить данные
+    request.open('GET', 'todos.json'); //первый аргумен это тип запроса, второй аргумент это откуда мы хотим получить запрос, конечная точка откуда хотим получить данные
     request.send();
 
 };
@@ -30,3 +32,6 @@ getTodos((err, data) => {
 
 console.log(3);
 console.log(4);
+
+
+/*Jason выглядит как массив объектов нам не нужно помещать его в строковый синтаксис, Теперь разница между тем, когда мы пишем объекты Jason и обычные объекты JavaScript, состоит в том, что все наши ключи должны идти в двойных кавычках, а когда мы используем строку в качестве значения, они также должны идти в двойных кавычках.*/
