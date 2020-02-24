183227fa94e57fe56ec44761baf27d9b3bb79e10
Ivan <ivan.lakhanin@gmail.com>
2020-02-24 15:42:58 +0200

    Object Oriented JavaScript - Constructors (under the hood)

--- lessons/sandbox.js
+++ lessons/sandbox.js
@@ -1,57 +1,23 @@
-//конструктор( типа шаблона) по которому мы создаём новые обьекты
-
-class User {
-    constructor(username, email) {
-        //set up properties
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-    login() {
-        console.log(`${this.username} just logged in`);
-        return this;
-    }
-    logout() {
-        console.log(`${this.username} just logged out`);
-        return this;
-    }
-    inScore() {
-        this.score += 1;
-        console.log(`${this.username} has a score of ${this.score}`);
-        return this;
-    }
-}
-//extend говорит, что хорошо, мы найдем класс User и автоматически унаследуем все методы и свойства от класса User
-class Admin extends User {
-    constructor(username, email, title) {
-        //когда используем super в нутри конструктора он ищет родительский класс (User) и смотрит в нутрь этого конструктора для использования свойств этого конструктора
-        super(username, email);
-        this.title = title;
-
-    }
-    deleteUser(user) {
-        users = users.filter(u => u.username !== user.username);
-    }
+//constructor functiond
+//Tак мы создаем конструкцию функции для создания новых объектов без использования класса.
+function User(username, email) {
+    this.username = username;
+    this.email = email;
+    this.login = function () {
+        console.log(`${this.username} has logged in`)
+    };
 }
 
-//сами обьекты но с разными свойствами
+// class User {
+//     constructor(username, email) {
+//         //set up properties
+//         this.username = username;
+//         this.email = email;
+//     }
+// }
+
 const userOne = new User('mario', 'mario@gmail.com');
 const userTwo = new User('luigi', 'luigi@gmail.com');
-const userThree = new Admin('shaun', 'shaun@gmail.com', 'matherFucker');
 
-console.log(userOne, userTwo, userThree);
-
-
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
-
-
-let users = [userOne, userTwo, userThree];
-console.log(users);
-
-userThree.deleteUser(userTwo);
-console.log(users);
-
-console.log(userThree);
+console.log(userOne, userTwo);
+userOne.login();
