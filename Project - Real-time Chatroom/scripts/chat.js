b3eceae4cf9a743cdef0066479c995f8a3363b5e
Ivan <ivan.lakhanin@gmail.com>
2020-02-25 19:09:08 +0200

    Chatroom - Updating the Room & Username

--- Project - Real-time Chatroom/scripts/chat.js
+++ Project - Real-time Chatroom/scripts/chat.js
@@ -9,6 +9,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat(message) {
         //format a chat object
@@ -26,7 +27,7 @@ class Chatroom {
         return response;
     }
     getChats(callback) {
-        this.chats
+        this.unsub = this.chats
             .where('room', '==', this.room)//этот метод в основном позволит нам получить документы из определенной коллекции, где выполняется определенное условие.
             .orderBy('created_at')
             .onSnapshot(snapshot => {
@@ -38,11 +39,30 @@ class Chatroom {
                 });
             });
     }
+    updateName(username) {
+        this.username = username;
+    }
+    updateRoom(room) {
+        this.room = room;
+        console.log('room updated')
+        if (this.unsub) {
+            this.unsub();
+        }
+    }
 }
 
 const chatroom = new Chatroom('general', 'shaun');
 
-
 chatroom.getChats((data) => {
     console.log(data);
 });
+
+setTimeout(() => {
+    chatroom.updateRoom('gaming');
+    chatroom.updateName('youshi');
+    chatroom.getChats((data) => {
+        console.log(data);
+    });
+    chatroom.addChat('hello dicks');
+
+}, 3000)
