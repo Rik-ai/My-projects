d51447b792689d7fcb36cf36aa816972be8d2dc2
Ivan <ivan.lakhanin@gmail.com>
2020-02-26 17:18:38 +0200

    ES6 Features - Symbols

--- lessons/sandbox.js
+++ lessons/sandbox.js
@@ -1,35 +1,17 @@
-//sets
-const namesArray = ['ryu', 'chun-li', 'ryu', 'shaun'];
-console.log(namesArray);
+const symbolOne = Symbol('a generic name');
+const symbolTwo = Symbol('a generic name');
 
-//const namesSet = new Set(['ryu', 'chun-li', 'ryu', 'shaun']);
-// const namesSet = new Set(namesArray);
-// console.log(namesSet);
+console.log(symbolOne, symbolTwo, typeof symbolOne);
+console.log(symbolOne == symbolTwo); //символы не могут быть одинаковыми даже если выглядят одинаково
 
 
-// const uniqueNames = [...namesSet];
-const uniqueNames = [...new Set(namesArray)]; 1
-console.log(uniqueNames);
+const ninja = {};
 
-const ages = new Set();
-ages.add(20);
-ages.add(25).add(30);//В сете не может быть двух одинаковых элементов
-ages.add(25);
-ages.delete(25);
-console.log(ages, ages.size);
-console.log(ages.has(30), ages.has(25));
+ninja.age = 30;
+ninja['belt'] = 'orange';
+ninja['belt'] = 'black';
 
+ninja[symbolOne] = 'ryu';
+ninja[symbolTwo] = 'shaun';
 
-ages.clear();
-console.log(ages);
-
-const ninjas = new Set([
-    { name: 'shaun', age: 30 },
-    { name: 'crystal', age: 25 },
-    { name: 'chun-li', age: 32 }
-]);
-ninjas.forEach(ninja => {
-    console.log(ninja.name, ninja.age);
-});
-
-
+console.log(ninja);
