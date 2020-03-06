fe30547972595f4bf009fb873258c0d8a89734fd
Ivan <ivan.lakhanin@gmail.com>
2020-03-06 16:37:55 +0200

    things

--- lessons/sandbox.js
+++ lessons/sandbox.js
@@ -1,17 +1,5 @@
-const symbolOne = Symbol('a generic name');
-const symbolTwo = Symbol('a generic name');
+const greet = (name) => {
+    console.log(`hello ${name}`);
+};
 
-console.log(symbolOne, symbolTwo, typeof symbolOne);
-console.log(symbolOne == symbolTwo); //символы не могут быть одинаковыми даже если выглядят одинаково
-
-
-const ninja = {};
-
-ninja.age = 30;
-ninja['belt'] = 'orange';
-ninja['belt'] = 'black';
-
-ninja[symbolOne] = 'ryu';
-ninja[symbolTwo] = 'shaun';
-
-console.log(ninja);
+greet();
