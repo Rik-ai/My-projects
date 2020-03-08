39956b86c3857dfef7f218162a3d237c28f0e51a
Ivan <ivan.lakhanin@gmail.com>
2020-03-08 20:43:36 +0200

    React - Работа с условными операторами

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -5,57 +5,55 @@ import Car from './Car/Car'
 
 class App extends Component {
 
-  state = { //компонент который описывает состояние в котором находится данный обьект
+  state = {
     cars: [
-      { name: 'Ford', year: '2018' },
-      { name: 'Ferrary', year: '2016' },
-      { name: 'Porshe', year: '2010' }
+      { name: 'Ford', year: 2018 },
+      { name: 'Audi', year: 2016 },
+      { name: 'Mazda', year: 2010 }
     ],
-    pageTitle: 'React components'
+    pageTitle: 'React components',
+    showCars: false
   }
 
-  changeTitleHandler = (newTitle) => {
+  toggleCarsHandler = () => {
     this.setState({
-      pageTitle: newTitle
+      showCars: !this.state.showCars
     })
   }
 
-  handleInput = (e) => {
-    this.setState({
-      pageTitle: e.target.value
-    })
+  changeTitleHandler = pageTitle => {
+    this.setState({ pageTitle })
   }
-
-
-
 
   render() {
     const divStyle = {
-      'textAlign': 'center'
+      textAlign: 'center'
     }
 
+    let cars = null
 
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onChangeTitle={() => this.changeTitleHandler(car.name)}
+          />
+        )
+      })
+    }
 
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
-        <input type='text' onChange={this.handleInput} />
+        <button
+          onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
 
-        <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change Title</button>
-
-        {this.state.cars.map((car, index) => {
-          return (
-            <Car
-              key={index}
-              name={car.name}
-              year={car.year}
-              onChangeTitle={() => {
-                this.changeTitleHandler(car.name)
-              }}
-            />
-          )
-        })}
+        {cars}
       </div>
     );
   }
