4f2442b294d16d178691baa05f093c7a382f7437
Ivan <ivan.lakhanin@gmail.com>
2020-03-08 18:41:24 +0200

    React - Inline-стили

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const divStyle = { //так можно стилизовать обьект в нутри функции
+      'textAlign': 'center'
+    }
     return (
-      <div className="App">
-        <h1>Hello world !</h1>
+      <div style={divStyle}>
+        {/* правильная запись для стилизации отдельных элементов */}
+        <h1 style={{ color: 'blue', 'fontSize': '20px' }}>Hello world !</h1>
       </div>
-      <p>Hello</p> //вывести параграф не получится так как должен быть корневой элемент типа <div></div>
     );
   }
 }
