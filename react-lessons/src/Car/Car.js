b6b6629f572ee82dedcb393cb1f103bd449a70d8
Ivan <ivan.lakhanin@gmail.com>
2020-03-08 18:58:49 +0200

    React - Вывод динамических данных

--- react-lessons/src/Car/Car.js
+++ react-lessons/src/Car/Car.js
@@ -1,32 +1,8 @@
 import React, { Component } from 'react';
 
-//Слишком длинная запись
-// function Car() {
-//     return (
-//         <h2>This is car component</h2>
-//     )
-// }
-
-//Запись по короче
-// const Car = () => {
-//     return (
-//         <h2>This is car component</h2>
-//     )
-// }
-
-//Короткая запись
-// const Car = () => (
-//     <div>
-//         This is car component
-//     <strong> Test</strong>
-//     </div>
-// )
-//export default Car
-
-//Самая короткая запись
 export default () => (
     <div>
-        This is car component
-   <strong> Test</strong>
+        <p>This is car component</p>
+        <p>Nomber: <strong>{Math.round(Math.random() * 100)}</strong></p>
     </div>
 )
