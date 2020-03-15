a3f56dea37b7effb835fdd70850208b486c85b00
Ivan <ivan.lakhanin@gmail.com>
2020-03-15 13:45:39 +0200

    relise v1

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Car from './Car/Car'
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 import Counter from './Counter/Counter'
