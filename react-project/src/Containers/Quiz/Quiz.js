e31ddb7768375afefeab7e313502a8beca3dc1f8
Ivan <ivan.lakhanin@gmail.com>
2020-03-18 09:49:37 +0200

    ReactProject - Отображение состояния ответа

--- react-project/src/Containers/Quiz/Quiz.js
+++ react-project/src/Containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import ActiveQuiz from '../../Components/AtiveQuiz/ActiveQuiz'
 class Quiz extends Component {
     state = {
         activeQuestion: 0,
+        answerState: null, //{[id]:'success' 'error'}
         quiz: [
             {
                 question: 'Какого хрена ?',
@@ -32,11 +33,36 @@ class Quiz extends Component {
     }
 
     onAnswerClickHandler = answerId => {
-        console.log('Answer Id: ', answerId)
 
-        this.setState({
-            activeQuestion: this.state.activeQuestion + 1
-        })
+        const question = this.state.quiz[this.state.activeQuestion]
+
+        if (question.rightAnswerId === answerId) {
+            this.setState({
+                answerState: { [answerId]: 'success' }
+            })
+
+            const timeout = window.setTimeout(() => {
+                if (this.isQuizFinished()) {
+                    console.log('Finished')
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+                window.clearTimeout(timeout)
+            }, 1000)
+
+
+        } else {
+            this.setState({
+                answerState: { [answerId]: 'error' }
+            })
+        }
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
 
@@ -54,6 +80,7 @@ class Quiz extends Component {
                         onAnswerClick={this.onAnswerClickHandler}
                         quizLength={this.state.quiz.length}
                         answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
                     />
                 </div>
             </div>
