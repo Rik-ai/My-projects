175d81fc140c120737bdb4516b7c289c9dc95495
Ivan <ivan.lakhanin@gmail.com>
2020-03-21 18:06:24 +0200

    React Router - Регистрация роута

--- react-lessons2/src/App.js
+++ react-lessons2/src/App.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './App.scss'
+//можем регать различные роуты внутри реакт прилажения
+import { Route } from 'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
 
@@ -19,7 +21,11 @@ class App extends Component {
           </ul>
         </nav>
 
-        <hr/>
+        <hr />
+        {/*Path - путь на который должен откликатся данный роут */}
+        {/* Exact - рендерит данный роут только если адресс полностью совпадает с тем что зареган в пути */}
+        <Route path='/' exact render={() => <h1>Home Page</h1>} />
+
         <About />
 
         <Cars />
