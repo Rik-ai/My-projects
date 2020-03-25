43b316ad946e2cc1867046dbb7188d9748ae2dbf
Ivan <ivan.lakhanin@gmail.com>
2020-03-25 15:44:48 +0200

    ReactRouter - Роутинг и функциональные компоненты

--- react-lessons2/src/Cars/Car/Car.js
+++ react-lessons2/src/Cars/Car/Car.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Car.scss'
+// с помощью withRouter мы получим доступ ко всем параметрам Car, оборачивает функциональные компоненты и добовляет им как свойство все параметры от Роутера
+import { withRouter } from 'react-router-dom'
 
 
 const Car = props => {
-  console.log(props)
   return (
-    <div className={'Car'}>
+    <div className={'Car'}
+      onClick={() => props.history.push('/cars/' + props.name.toLowerCase())}
+    >
       <h3>Сar name: {props.name}</h3>
       <p>Year: <strong>{props.year}</strong></p>
     </div>
   )
 }
 
-export default Car
+export default withRouter(Car)
