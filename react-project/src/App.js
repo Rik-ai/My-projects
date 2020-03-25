66648a80540e9523a189a54c0b9d66a1517aa0ee
Ivan <ivan.lakhanin@gmail.com>
2020-03-25 16:38:49 +0200

    RoutingPL - Создание страниц

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
+import { Route, Switch } from 'react-router-dom'
+import QuizList from './Containers/QuizList/QuizList'
+import Auth from './Containers/Auth/Auth'
+import QuizCreator from './Containers/QuizCreator/QuizCreator'
 import Quiz from './Containers/Quiz/Quiz'
+
 
 
 
@@ -8,9 +13,12 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
-
-
+        <Switch>
+          <Route path='/auth' component={Auth} />
+          <Route path='/quiz-creator' component={QuizCreator} />
+          <Route path='/quiz/:id' component={Quiz} />
+          <Route path='/' component={QuizList} />
+        </Switch>
       </Layout>
     )
 
