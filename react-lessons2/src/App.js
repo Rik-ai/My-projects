3f1906ab7f62816936af0c92bb2aa08988456c34
Ivan <ivan.lakhanin@gmail.com>
2020-03-25 15:01:08 +0200

    React Router - Роутинг и компоненты

--- react-lessons2/src/App.js
+++ react-lessons2/src/App.js
@@ -2,34 +2,38 @@ import React, { Component } from 'react'
 import './App.scss'
 //можем регать различные роуты внутри реакт прилажения
 import { Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
 
 class App extends Component {
   render() {
-
     return (
-      <div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/cars">Cars</a>
-            </li>
-          </ul>
-        </nav>
+      <BrowserRouter>
+        <div>
+          <nav className="nav">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/cars">Cars</a>
+              </li>
+            </ul>
+          </nav>
 
-        <hr />
-        {/*Path - путь на который должен откликатся данный роут */}
-        {/* Exact - рендерит данный роут только если адресс полностью совпадает с тем что зареган в пути */}
-        <Route path="/" exact render={() => <h1>Home Page</h1>} />
-        <Route path="/about" component={About} />
-        <Route path="/cars" component={Cars} />
+          <hr />
+          {/*Path - путь на который должен откликатся данный роут */}
+          {/* Exact - рендерит данный роут только если адресс полностью совпадает с тем что зареган в пути */}
+          <Route path="/" exact render={() => <h1>Home Page</h1>} />
+          <Route path="/about" component={About} />
+          <Route path="/cars" component={Cars} />
 
-
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
