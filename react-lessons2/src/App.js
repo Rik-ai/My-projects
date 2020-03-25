5a279f25fd4aa3a9ff8a5cd03bb4a86cdd68b64f
Ivan <ivan.lakhanin@gmail.com>
2020-03-25 16:01:52 +0200

    ReactRouter -  Динамические роуты

--- react-lessons2/src/App.js
+++ react-lessons2/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import './App.scss'
 //можем регать различные роуты внутри реакт прилажения
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
 
 class App extends Component {
   render() {
@@ -39,10 +40,14 @@ class App extends Component {
           <hr />
           {/*Path - путь на который должен откликатся данный роут */}
           {/* Exact - рендерит данный роут только если адресс полностью совпадает с тем что зареган в пути */}
-          <Route path="/" exact render={() => <h1>Home Page</h1>} />
-          <Route path="/about" component={About} />
-          <Route path="/cars" component={Cars} />
+          {/* Switch - аналогия exact */}
+          <Switch>
+            <Route path="/" exact render={() => <h1>Home Page</h1>} />
+            <Route path="/about" component={About} />
+            <Route path="/cars/:name" component={CarDetail} /
+            ><Route path="/cars" component={Cars} />
 
+          </Switch>
         </div>
       </BrowserRouter>
     );
