c64c8299c18593d4068609ffe290a618e8d6ae39
Ivan <ivan.lakhanin@gmail.com>
2020-03-26 19:29:06 +0200

    Forms - Валидация

--- react-project/src/Containers/Auth/Auth.js
+++ react-project/src/Containers/Auth/Auth.js
@@ -3,7 +3,46 @@ import classes from './Auth.module.css'
 import Button from '../../Components/Ui/Button/Button'
 import Input from '../../Components/Ui/Input/Input'
 
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
 export default class Auth extends Component {
+
+    // Валидация
+    state = {
+        formControls: {
+            email: {
+                valu: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введи нормальный email ска',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                valu: '',
+                type: 'password',
+                label: 'Пароль',
+                errorMessage: 'Введи нормальный пароль ска',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
 
     loginHandler = () => {
 
@@ -19,6 +58,60 @@ export default class Auth extends Component {
         e.preventDefault()
     }
 
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+        if (validation.email) {
+            isValid = validateEmail(value) && isValid
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+    }
+
+    onChangeHandler = (e, controlName) => {
+
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
+
+        control.value = e.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        this.setState({
+            formControls
+        })
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    shouldValidate={!!control.validation}
+                    errorMessage={control.errorMessage}
+                    onChange={e => this.onChangeHandler(e, controlName)}
+                />
+            )
+        })
+    }
 
     render() {
         return (
@@ -27,27 +120,21 @@ export default class Auth extends Component {
                     <h1>Авторизация</h1>
 
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                        <Input
-                            label='Email'
-                        />
 
-                        <Input
-                            label='Password'
-                            errorMessage={'TEST'}
-                        />
+                        {this.renderInputs()}
 
                         <Button
 
                             type='succes'
                             onClick={this.loginHandler}
                         >
-                            Login
+                            Войти
                         </Button>
                         <Button
                             type='primary'
                             onClick={this.registerHandler}
                         >
-                            Register
+                            Зарегистрироваться
                         </Button>
                     </form>
                 </div>
