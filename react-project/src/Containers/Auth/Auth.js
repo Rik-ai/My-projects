7441d9774e55d5192df6cce00c961eb395502cff
Ivan <ivan.lakhanin@gmail.com>
2020-03-26 14:55:37 +0200

    Forms - Страница авторизации

--- react-project/src/Containers/Auth/Auth.js
+++ react-project/src/Containers/Auth/Auth.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
+import classes from './Auth.module.css'
+import Button from '../../Components/Ui/Button/Button'
 
 export default class Auth extends Component {
+
+    loginHandler = () => {
+
+
+    }
+
+    registerHandler = () => {
+
+
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+    }
+
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <input type='text' />
+                        <input type='text' />
+                        <Button
+                            type='succes'
+                            onClick={this.loginHandler}
+                        >
+                            Login
+                        </Button>
+                        <Button
+                            type='primary'
+                            onClick={this.registerHandler}
+                        >
+                            Register
+                        </Button>
+                    </form>
+                </div>
 
             </div>
         )
