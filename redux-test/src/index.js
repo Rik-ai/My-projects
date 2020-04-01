54758f41eb4e47d777d7fff78980ca3a37f985f9
Ivan <ivan.lakhanin@gmail.com>
2020-04-01 15:22:53 +0300

    Redux - Как работает Middleware

--- redux-test/src/index.js
+++ redux-test/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './redux/rootReducer'
 
-const store = createStore(rootReducer)
+
+// function loggerMiddleware(store) { //Middleware - функция которая добавляет определённый функционал чему-то (на пример Redux)
+//   return function (next) {
+//     return function (action) {
+//       const result = next(action)
+//       console.log('Middleware', store.getState())
+//       return result
+//     }
+//   }
+// }
+//То же что и выше но проще 
+const loggerMiddleware = store => next => action => {
+  const result = next(action)
+  console.log('Middleware', store.getState())
+  return result
+}
+
+
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware))
 
 const app = (
   <Provider store={store}>
